0c1ae4b81d3fde69d65e1f128b57ed66dfc2c3bb
grace <C64086117@ncku.edu.tw>
2020-08-24 09:13:16 +0800

    詐騙破解的部分

---
+++ package/mischieve.py
@@ -0,0 +1,25 @@
+#詐騙破解
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import csv
+################chrome_headless######################
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+#####################################################
+get_title_and_href_dict = {}
+for i in range(1,4,1):
+    url = 'https://www.mygopen.com/search/label/%E8%A9%90%E9%A8%99#archive-page-'+str(i)
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    driver.refresh()
+    WebDriverWait(driver,10).until(lambda d: d.find_elements_by_css_selector('.item-content .item-title a'))
+        # print(get_title_and_href_dict)
+    with open('mygopen_mischieve.csv','a',encoding = 'utf-8',newline='')as csvfile:
+        fieldNames = ['title','href']
+        writer = csv.DictWriter(csvfile,fieldnames= fieldNames,dialect='excel')
+        writer.writeheader()
+        a_tag = driver.find_elements_by_css_selector('.item-content .item-title a')
+        for j in range(len(a_tag)):
+            get_title_and_href_dict.update({'title':a_tag[j].text,'href':a_tag[j].get_attribute('href')})
+            writer.writerow(get_title_and_href_dict)
